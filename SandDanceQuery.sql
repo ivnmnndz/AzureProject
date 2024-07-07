a2b4b50502f97b04cbbfca60a7072b80047284dc
ivnmnndz <12gues@gmail.com>
2024-07-07 19:26:17 -0400

    new query

--- SandDanceQuery.sql
+++ SandDanceQuery.sql
@@ -1,21 +1,9 @@
 USE EVPopulation;
 
 SELECT 
-    Cleaned_Electric_Utility,
-    COUNT(*) AS Count
-FROM (
-    SELECT 
-        CASE 
-            WHEN Electric_Utility LIKE 'BONNEVILLE POWER ADMINISTRATION%' THEN 'BONNEVILLE POWER ADMINISTRATION'
-            WHEN Electric_Utility LIKE 'PUGET%' THEN 'PUGET'
-            WHEN Electric_Utility LIKE 'CITY OF SEATTLE%' THEN 'CITY OF SEATTLE'
-            ELSE Electric_Utility
-        END AS Cleaned_Electric_Utility
-    FROM Electric_Vehicle_Population_Data
-    WHERE Electric_Utility IS NOT NULL
-) AS UtilityCategories
-GROUP BY Cleaned_Electric_Utility
-HAVING COUNT(*) > 3000
-ORDER BY Count DESC;
-
+    [Clean_Alternative_Fuel_Vehicle_CAFV_Eligibility] AS [CAFV Eligibility], 
+    COUNT(*) AS NumVehicles
+FROM [EVPopulation].[dbo].[Electric_Vehicle_Population_Data]
+GROUP BY [Clean_Alternative_Fuel_Vehicle_CAFV_Eligibility]
+ORDER BY NumVehicles DESC;
 
